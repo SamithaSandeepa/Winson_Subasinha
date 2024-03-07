35b830a68dece16b21122bd827cf3cc624d5f322
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2024-03-07 18:02:19 +0530

    first update

---
+++ jsonfile/imagesData.js
@@ -0,0 +1,59 @@
+const imagesData = [
+  {
+    id: 1,
+    category: ["fashion", "urban"],
+    imageUrl: "images/portfolio-item1.jpg",
+    thumbUrl: "images/portfolio-thumb-1.jpg",
+    title: "Sample Caption goes here for Portfolio Item 1.",
+  },
+  {
+    id: 2,
+    category: ["fashion", "portrait", "events"],
+    imageUrl: "images/portfolio-item2.jpg",
+    thumbUrl: "images/portfolio-thumb-2.jpg",
+    title: "Sample Caption goes here for Portfolio Item 2.",
+  },
+  // Add more items as needed...
+  {
+    id: 3,
+    category: ["fashion", "urban"],
+    imageUrl: "images/portfolio-item3.jpg",
+    thumbUrl: "images/portfolio-thumb-3.jpg",
+    title: "Sample Caption goes here for Portfolio Item 3.",
+  },
+  {
+    id: 4,
+    category: ["fashion", "portrait", "events"],
+    imageUrl: "images/portfolio-item4.jpg",
+    thumbUrl: "images/portfolio-thumb-4.jpg",
+    title: "Sample Caption goes here for Portfolio Item 4.",
+  },
+  {
+    id: 5,
+    category: ["fashion", "urban"],
+    imageUrl: "images/portfolio-item5.jpg",
+    thumbUrl: "images/portfolio-thumb-5.jpg",
+    title: "Sample Caption goes here for Portfolio Item 5.",
+  },
+  {
+    id: 6,
+    category: ["fashion", "portrait", "events"],
+    imageUrl: "images/portfolio-item6.jpg",
+    thumbUrl: "images/portfolio-thumb-6.jpg",
+    title: "Sample Caption goes here for Portfolio Item 6.",
+  },
+  {
+    id: 7,
+    category: ["fashion", "urban"],
+    imageUrl: "images/portfolio-item7.jpg",
+    thumbUrl: "images/portfolio-thumb-7.jpg",
+    title: "Sample Caption goes here for Portfolio Item 7.",
+  },
+  {
+    id: 8,
+    category: ["fashion", "portrait", "events"],
+    imageUrl: "images/portfolio-item8.jpg",
+    thumbUrl: "images/portfolio-thumb-8.jpg",
+    title: "Sample Caption goes here for Portfolio Item 8.",
+  },
+];
