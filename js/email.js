9b0f84e11f3c0be250d5f13162b53ec5d423d87e
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2024-03-25 14:10:31 +0530

    add blog

---
+++ js/email.js
@@ -0,0 +1,18 @@
+function sendEmail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceID = "service_6tkfa8p";
+  const templateID = "template_89jnmts";
+
+  emailjs.send(serviceID, templateID, params).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+}
